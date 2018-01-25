bc4f4c62baad6870bfec0b384206d80407db7e83
itggot-felix-hornfeldt <felix.hornfeldt@itggot.se>
2018-01-25 17:37:55 +0100

    javascript working but not with apache

--- site/js/main.js
+++ site/js/main.js
@@ -1,3 +1,4 @@
-function showDropdown() {
-    
+function showDropDown() {
+    document.querySelector('#headerContentBox').style.display = "none";
+    document.querySelector('#headerDropdownContent').style.display = "grid";
 }
