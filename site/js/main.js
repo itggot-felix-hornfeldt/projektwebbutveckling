60d8bf0190a9d4787b074d12584aa58ff92a92cf
itggot-felix-hornfeldt <felix.hornfeldt@itggot.se>
2018-01-25 21:55:30 +0100

    dropdown html, css and js done

--- site/js/main.js
+++ site/js/main.js
@@ -1,4 +1,13 @@
-function showDropDown() {
-    document.querySelector('#headerContentBox').style.display = "none";
-    document.querySelector('#headerDropdownContent').style.display = "grid";
+function showDropdown() {
+    document.querySelector('.headerContentBox').style.display = "none";
+    document.querySelector('main').style.display = "none";
+    document.querySelector('footer').style.display = "none";
+    document.querySelector('.headerDropdownContent').style.display = "grid";
+}
+
+function removeDropdown() {
+    document.querySelector('.headerContentBox').style.display = "grid";
+    document.querySelector('main').style.display = "grid";
+    document.querySelector('footer').style.display = "grid";
+    document.querySelector('.headerDropdownContent').style.display = "none";
 }
