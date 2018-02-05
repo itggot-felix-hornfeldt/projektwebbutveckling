f9d89600eed0e0541003fa8eddeba878d24c6ac0
itggot-felix-hornfeldt <felix.hornfeldt@itggot.se>
2018-02-05 17:25:26 +0100

    training type, type of training and what week, javascript done for these

--- site/js/book.js
+++ site/js/book.js
@@ -1,4 +1,4 @@
-function choiceGroup() {
+function choiceTrainingGroup() {
     document.querySelector('#siteBookParagraphFirstChoice').innerHTML = "Gruppträning";
     document.querySelector('.siteBookChoiceBoxTraining').style.display = "none";
     document.querySelector('.siteBookChoiceBoxType').style.display = "grid";
@@ -8,4 +8,33 @@ function choiceTypeCardio() {
     document.querySelector('#siteBookParagraphSecondChoice').innerHTML = "Kondition";
     document.querySelector('.siteBookChoiceBoxType').style.display = "none";
     document.querySelector('.siteBookChoiceBoxWeek').style.display = "grid";
+    document.querySelector('.siteBookFirstArrowRight').style.display = "grid";
+}
+
+function choiceTypeStrength() {
+    document.querySelector('#siteBookParagraphSecondChoice').innerHTML = "Styrka";
+    document.querySelector('.siteBookChoiceBoxType').style.display = "none";
+    document.querySelector('.siteBookChoiceBoxWeek').style.display = "grid";
+    document.querySelector('.siteBookFirstArrowRight').style.display = "grid";
+}
+
+function choiceTypeYoga() {
+    document.querySelector('#siteBookParagraphSecondChoice').innerHTML = "Yoga";
+    document.querySelector('.siteBookChoiceBoxType').style.display = "none";
+    document.querySelector('.siteBookChoiceBoxWeek').style.display = "grid";
+    document.querySelector('.siteBookFirstArrowRight').style.display = "grid";
+}
+
+function choiceThisWeek() {
+    document.querySelector('#siteBookParagraphThirdChoice').innerHTML = "Denna veckan";
+    document.querySelector('.siteBookChoiceBoxWeek').style.display = "none";
+    document.querySelector('.siteBookChoiceBoxTimes').style.display = "grid";
+    document.querySelector('.siteBookSecondArrowRight').style.display = "grid";
+}
+
+function choiceNextWeek() {
+    document.querySelector('#siteBookParagraphThirdChoice').innerHTML = "Nästa vecka";
+    document.querySelector('.siteBookChoiceBoxWeek').style.display = "none";
+    document.querySelector('.siteBookChoiceBoxTimes').style.display = "grid";
+    document.querySelector('.siteBookSecondArrowRight').style.display = "grid";
 }
