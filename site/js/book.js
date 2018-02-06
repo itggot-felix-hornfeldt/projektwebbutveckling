6a062b531fad1d422e501bc82b7eb94691ef0b3c
itggot-felix-hornfeldt <felix.hornfeldt@itggot.se>
2018-02-06 20:53:47 +0100

    all but done element class done

--- site/js/book.js
+++ site/js/book.js
@@ -1,21 +1,26 @@
+let bookInfoArr = [];
+
 function choiceTrainingGroup() {
     document.querySelector('#siteBookParagraphFirstChoice').innerHTML = "Gruppträning";
     document.querySelector('.siteBookChoiceBoxTraining').style.display = "none";
     document.querySelector('.siteBookChoiceBoxType').style.display = "grid";
+    bookInfoArr.push('Gruppträning');
 }
 
-function choiceTypeCardio() {
-    document.querySelector('#siteBookParagraphSecondChoice').innerHTML = "Kondition";
-    document.querySelector('.siteBookChoiceBoxType').style.display = "none";
-    document.querySelector('.siteBookChoiceBoxWeek').style.display = "grid";
-    document.querySelector('.siteBookFirstArrowRight').style.display = "grid";
-}
+// function choiceTypeCardio() {
+//     document.querySelector('#siteBookParagraphSecondChoice').innerHTML = "Kondition";
+//     document.querySelector('.siteBookChoiceBoxType').style.display = "none";
+//     document.querySelector('.siteBookChoiceBoxWeek').style.display = "grid";
+//     document.querySelector('.siteBookFirstArrowRight').style.display = "grid";
+//     bookInfoArr.push('Konditions');
+// }
 
 function choiceTypeStrength() {
     document.querySelector('#siteBookParagraphSecondChoice').innerHTML = "Styrka";
     document.querySelector('.siteBookChoiceBoxType').style.display = "none";
     document.querySelector('.siteBookChoiceBoxWeek').style.display = "grid";
     document.querySelector('.siteBookFirstArrowRight').style.display = "grid";
+    bookInfoArr.push('Styrke');
 }
 
 function choiceTypeYoga() {
@@ -23,6 +28,7 @@ function choiceTypeYoga() {
     document.querySelector('.siteBookChoiceBoxType').style.display = "none";
     document.querySelector('.siteBookChoiceBoxWeek').style.display = "grid";
     document.querySelector('.siteBookFirstArrowRight').style.display = "grid";
+    bookInfoArr.push('Yoga');
 }
 
 function choiceThisWeek() {
@@ -30,18 +36,38 @@ function choiceThisWeek() {
     document.querySelector('.siteBookChoiceBoxWeek').style.display = "none";
     document.querySelector('.siteBookChoiceBoxTimes').style.display = "grid";
     document.querySelector('.siteBookSecondArrowRight').style.display = "grid";
+    bookInfoArr.push('nu på');
 }
 
-// function choiceNextWeek() {
-//     document.querySelector('#siteBookParagraphThirdChoice').innerHTML = "Nästa vecka";
-//     document.querySelector('.siteBookChoiceBoxWeek').style.display = "none";
-//     document.querySelector('.siteBookChoiceBoxTimes').style.display = "grid";
-//     document.querySelector('.siteBookSecondArrowRight').style.display = "grid";
-// }
+function choiceNextWeek() {
+    document.querySelector('#siteBookParagraphThirdChoice').innerHTML = "Nästa vecka";
+    document.querySelector('.siteBookChoiceBoxWeek').style.display = "none";
+    document.querySelector('.siteBookChoiceBoxTimes').style.display = "grid";
+    document.querySelector('.siteBookSecondArrowRight').style.display = "grid";
+    bookInfoArr.push('nästa');
+}
 
 function choiceTime() {
     document.querySelector('#siteBookParagraphForthChoice').innerHTML = "tis 17:30";
     document.querySelector('.siteBookChoiceBoxTimes').style.display = "none";
     document.querySelector('.siteBookChoiceBoxConformation').style.display = "grid";
     document.querySelector('.siteBookThirdArrowRight').style.display = "grid";
+    bookInfoArr.push('Tisdag', '17:30');
+    conformationInfo();
+}
+
+function conformationInfo() {
+    document.querySelector('#firstConformation').innerHTML = bookInfoArr[1];
+    document.querySelector('#secondConformation').innerHTML = bookInfoArr[0];
+    document.querySelector('#thirdConformation').innerHTML = bookInfoArr[2];
+    document.querySelector('#forthConformation').innerHTML = bookInfoArr[3];
+    document.querySelector('#fifthConformation').innerHTML = bookInfoArr[4];
+}
+
+function conformationYes() {
+    
+}
+
+function conformationNo() {
+    location.reload();
 }
